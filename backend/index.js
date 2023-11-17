a75bce2cccc1f3ff11be9b58348f04de91b92895
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2023-11-17 13:21:23 +0200

    start

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,30 @@
-console.log("Hello from the backend");
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+const data = [];
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.get("/api/sendData", function (req, res) {
+  const temperature = req.query.temperature;
+  const humidity = req.query.humidity;
+  const id = req.query.id;
+
+  const dataReceived = {
+    temperature: temperature,
+    humidity: humidity,
+    id: id,
+  };
+
+  data.push(dataReceived);
+
+  res.json(dataReceived);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
