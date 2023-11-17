f4ba12d39d8df7747872fc1d5f5b63722dadd823
Bugulet <eusebiu.buga@gmail.com>
2023-11-17 20:52:15 +0200

    esp final

--- hardware/main_esp_sketch/main_esp_sketch.ino
+++ hardware/main_esp_sketch/main_esp_sketch.ino
@@ -14,33 +14,51 @@ void setup() {
   Serial.begin(115200); 
 
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
+  // Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    // Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println("");
+  // Serial.print("Connected to WiFi network with IP Address: ");
+  // Serial.println(WiFi.localIP());
  
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  // Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-int humidity=100;
+bool stringComplete=false;
+String inputString="";
+
 void loop() {
-  // Send an HTTP POST request depending on timerDelay
-  if ((millis() - lastTime) > timerDelay) {
+
+  
+  if(Serial.available()) {
+    inputString = Serial.readStringUntil('\n');
+    stringComplete=true;
+  }
+
+  // Send an HTTP POST request depending on timerDelay and if we read anything
+  if (stringComplete && (millis() - lastTime) > timerDelay) {
+    Serial.println(inputString);
+    int temperature=0;
+    int humidity=0;
+    int emission=0;
+
+    sscanf(inputString.c_str(), "%d %d %d", &temperature, &humidity,&emission);
     //Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
 
-      String serverPath = serverName + "?temperature=25&humidity="+String(humidity);
-      humidity++;
+      String serverPath = serverName 
+            +String("?temperature=")+String(temperature)
+            +String("&humidity=")+String(humidity)
+            +String("&emission=")+String(emission);
       // Your Domain name with URL path or IP address with path
       http.begin(client, serverPath.c_str());
   
       int httpResponseCode = http.GET();
+
       
       if (httpResponseCode>0) {
         Serial.print("HTTP Response code: ");
@@ -54,6 +72,7 @@ void loop() {
       }
       // Free resources
       http.end();
+      stringComplete=false;
     }
     else {
       Serial.println("WiFi Disconnected");
